aa12f25f1f784a1c0017df744760b0e095876e79
funashi <pinkgreen0304@gmail.com>
2018-07-02 21:23:49 +0900

    modified serializer

--- ladder/serializers.py
+++ ladder/serializers.py
@@ -41,7 +41,7 @@ class UserSerializer(serializers.ModelSerializer):
         serialize = {}
         list = []
         for ladder in instance.get_my_link():
-            serialize = {'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
+            serialize = {'id':ladder.pk,'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
             list.append(serialize)
         return list
 
@@ -49,7 +49,7 @@ class UserSerializer(serializers.ModelSerializer):
         serialize = {}
         list = []
         for ladder in instance.get_my_ladders():
-            serialize = {'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
+            serialize = {'id':ladder.pk,'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
             list.append(serialize)
         return list
 
@@ -71,21 +71,21 @@ class LadderSerializer(serializers.ModelSerializer):
         serialize ={}
         list = []
         for unit in instance.get_unit():
-            serialize = {'title':unit.title,'description':unit.description,'ladder':unit.ladder.title,'url':unit.url,'index':unit.index}
+            serialize = {'id':unit.pk,'title':unit.title,'description':unit.description,'ladder':unit.ladder.title,'url':unit.url,'index':unit.index}
             list.append(serialize)
         return list
 
     def get_recommended_prev_ladder(self,instance):
         ladder = instance.get_recommended_prev_ladder()
         if ladder:
-            return {'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
+            return {'id':ladder.pk,'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
         else:
             return None
 
     def get_recommended_next_ladder(self,instance):
         ladder = instance.get_recommended_next_ladder()
         if ladder:
-            return {'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
+            return {'id':ladder.pk,'title':ladder.title,'tags':ladder.tags.name,'creater':ladder.creater.name,'created_at':ladder.created_at}
         else:
             return None
 
