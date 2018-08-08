98a11fdd8e1d61044e55b84af0039a880260708b
funashi <pinkgreen0304@gmail.com>
2018-08-08 13:41:32 +0900

    modified create user

--- ladder/models.py
+++ ladder/models.py
@@ -51,14 +51,17 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
-        """スーパーユーザーは、is_staffとis_superuserをTrueに"""
+
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active',True)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active=True')
 
         return self._create_user(email, password, **extra_fields)
 
